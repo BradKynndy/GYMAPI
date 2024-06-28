8557bca1f14e120f0839d3e8b3c5b940209421ce
brad <bkynndy@gmail.com>
2024-06-28 05:30:55 -0300

    update

--- obj/Debug/net8.0/GymApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/GymApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975cd161536f01c49da2d52bdb43fa168e87299b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89163768e5ea2be6dc9c330ccbeb77ca77102764")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
